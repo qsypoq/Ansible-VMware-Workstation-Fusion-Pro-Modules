2263870e63c0deb939b1f70543cf42a2cfa9adc9
Adam Qsypoq Magnier <github@magnier.io>
2020-04-30 21:55:05 +0200

    allow ip:port variations and default values

--- library/vmware_workstation_clone.py
+++ library/vmware_workstation_clone.py
@@ -17,25 +17,52 @@ description:
     - "Clone VMware Workstation VM"
 
 options:
-    targetId:
+    targetVM:
         description:
             - This is the target VM to interact with
         required: true
+
+    newname: "KMS-Server-Clone"
+        description:
+            - This is the name of the cloned VM
+        required: true      
+
+    user: "workstation-api-user"
+        description:
+            - Your workstation API username
+        required: true
+
+    pass: "workstation-api-password"
+        description:
+            - Your workstation API password
+        required: true
+
+    apiurl: "http://127.0.0.1"
+        description:
+            - Your workstation API URL
+        required: false
+        default: "http://127.0.0.1"
+
+    apiport: "8697"
+        description:
+            - Your workstation API PORT
+        required: false
+        default: "8697"
 
 author:
     - Adam Magnier (@qsypoq)
 '''
 
 EXAMPLES = '''
-# Clone VM with ID 42
+# Clone VM with ID 42 as KMS-Server-Clone using API @ http://127.0.0.1:8697
 - name: "Start VM ID 42"
   vmware_workstation_clone:
     targetVM: "42"
-    newname: "Hello_World"
-    user: "api-user"
-    pass: "api-password"
-    apiurl: "http://127.0.0.1:8697/api/vms"
-
+    newname: "KMS-Server-Clone"
+    user: "workstation-api-user"
+    pass: "workstation-api-password"
+    apiurl: "http://127.0.0.1"
+    apiport: "8697"
 '''
 
 RETURN = '''
