f4558c76a2fd399be00a5988186a389259820929
Adam Qsypoq Magnier <github@magnier.io>
2020-05-01 17:29:17 +0200

    implemented adaptersmgmt

---
+++ library/vmware_workstation_adaptersmgmt.ps1
@@ -0,0 +1,104 @@
+#!powershell
+
+#Requires -Module Ansible.ModuleUtils.CommandUtil
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+$result = New-Object psobject @{
+    changed = $false
+}
+
+$params = Parse-Args -arguments $args -supports_check_mode $true
+
+$user =  Get-AnsibleParam -obj $params -name "user" -type "str" -failifempty $true
+$pass = Get-AnsibleParam -obj $params -name "pass" -type "str" -failifempty $true
+$targetVM = Get-AnsibleParam -obj $params -name "targetVM" -type "str" -failifempty $true
+$action = Get-AnsibleParam -obj $params -name "action" -type "str" -failifempty $true 
+
+$apiurl = Get-AnsibleParam -obj $params -name "apiurl" -type "str" -default "http://127.0.0.1" -failifempty $false 
+$apiport = Get-AnsibleParam -obj $params -name "apiport" -type "int" -default "8697" -failifempty $false
+
+if (($action -eq 'list' ) -Or ($action -eq 'create')) { 
+    $requesturl = "${apiurl}:${apiport}/api/vms/${targetVM}/nic"
+}
+if ($action -eq 'getip' ) { 
+    $requesturl = "${apiurl}:${apiport}/api/vms/${targetVM}/ip"
+}
+if (($action -eq 'update' ) -Or ($action -eq 'create')) {
+    $targetType = Get-AnsibleParam -obj $params -name "targetType" -type "str" -failifempty $true 
+}
+if ($targetType -eq 'custom' ) {
+    $targetVMNET = Get-AnsibleParam -obj $params -name "targetVMNET" -type "str" -failifempty $true
+}
+
+if (($action -eq 'update' ) -Or ($action -eq 'delete')) { 
+    $targetIndex = Get-AnsibleParam -obj $params -name "targetIndex" -type "int" -failifempty $true 
+    $requesturl = "${apiurl}:${apiport}/api/vms/${targetVM}/nic/${targetIndex}"
+}
+
+$pair = "${user}:${pass}"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$basicAuthValue = "Basic $base64"
+
+$headers = @{
+    'Authorization' =  $basicAuthValue;
+    'Content-Type' =  'application/vnd.vmware.vmw.rest-v1+json';
+    'Accept' = 'application/vnd.vmware.vmw.rest-v1+json';
+}
+
+if (($action -eq 'update' ) -Or ($action -eq 'create')) { 
+    $body = @{
+        "type" = $targetType;
+        "vmnet" = $targetVMNET
+    }      
+}
+
+$requestbody = ($body | ConvertTo-Json)
+
+if (($action -eq 'list' ) -Or ($action -eq 'getip')) { 
+    try {
+        $listrequest = Invoke-RestMethod -Uri $requesturl -Headers $headers -method 'Get'
+        $result.infos = $listrequest
+        $result.changed = $false;
+    }
+    catch {
+            Fail-Json $result "Request failed, please check your configuration"
+    }
+}
+
+if ($action -eq 'create' ) { 
+    try {
+        $createrequest = Invoke-RestMethod -Uri $requesturl -Headers $headers -method 'Post' -Body $requestbody
+        $result.infos = $createrequest
+        $result.changed = $true;
+    }
+    catch {
+            Fail-Json $result "Request failed, please check your configuration"
+    }
+}
+
+if ($action -eq 'update' ) { 
+    try {
+        $createrequest = Invoke-RestMethod -Uri $requesturl -Headers $headers -method 'Put' -Body $requestbody
+        $result.infos = $createrequest
+        $result.changed = $true;
+    }
+    catch {
+            Fail-Json $result "Request failed, please check your configuration"
+    }
+}
+
+if ($action -eq 'delete' ) { 
+    try {
+        $createrequest = Invoke-RestMethod -Uri $requesturl -Headers $headers -method 'DELETE'
+        $result.infos = $createrequest
+        $result.changed = $true;
+    }
+    catch {
+            Fail-Json $result "Request failed, please check your configuration"
+    }
+}
+
+Exit-Json $result;
---
+++ library/vmware_workstation_adaptersmgmt.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = r'''
+module: vmware_workstation_foldersmgmt
+
+short_description: Implement the Shared Folders Management part of the API
+
+version_added: "2.4"
+
+description:
+    - "Manage VMware Workstation Shared Folders"
+
+options:
+    targetVM:
+        description:
+            - This is the target VM to interact with
+        required: true
+
+    action: infos || create || delete || update
+        description:
+            - This is the action we want to do.
+        required: true   
+
+    targetFolder: "myFolderName"
+        description:
+            - Name of the shared folder
+        required: Only for create & update & delete
+
+    targetPath: C:\Users\qsypoq\Desktop\odbg110
+        description:
+            - Path of shared folder
+        required: Only for create & update
+    
+    user: "workstation-api-user"
+        description:
+            - Your workstation API username
+        required: true
+
+    pass: "workstation-api-password"
+        description:
+            - Your workstation API password
+        required: true
+
+    apiurl: "http://127.0.0.1"
+        description:
+            - Your workstation API URL
+        required: false
+        default: "http://127.0.0.1"
+
+    apiport: "8697"
+        description:
+            - Your workstation API PORT
+        required: false
+        default: "8697"
+
+author:
+    - Adam Magnier (@qsypoq)  
+'''
+
+EXAMPLES = r'''
+- name: "Create shared folder named ODBG110 on VM ID 42"
+  vmware_workstation_foldersmgmt:
+    targetVM: "42"
+    targetFolder: "ODBG110"
+    targetPath: C:\Users\qsypoq\Desktop\odbg110
+    action: "create"
+    user: "workstation-api-user"
+    pass: "workstation-api-password"
+    
+- name: "Delete shared folder named ODBG110 on VM ID 42"
+  vmware_workstation_foldersmgmt:
+    targetVM: "42"
+    targetFolder: "ODBG110"
+    action: "delete"
+    user: "workstation-api-user"
+    pass: "workstation-api-password"
+'''
+
+RETURN = r'''
+'''
