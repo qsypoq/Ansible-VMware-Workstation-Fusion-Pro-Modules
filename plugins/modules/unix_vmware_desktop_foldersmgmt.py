009c898ad42167b2ebd580d0428223a8310531c6
Markus Kraus <markus.kraus@gmail.com>
2020-05-13 14:06:45 -0400

    add parameter validate_certs

--- plugins/modules/unix_vmware_desktop_foldersmgmt.py
+++ plugins/modules/unix_vmware_desktop_foldersmgmt.py
@@ -70,6 +70,11 @@ options:
         required: false
         default: "8697"
 
+    validate_certs: "no || yes"
+        description:
+            - Validate Certificate it HTTPS connection
+        required: false
+
 author:
     - Adam Magnier (@qsypoq)  
 '''
@@ -165,6 +170,7 @@ def run_module():
         access=dict(type='str', required=False),
         api_url=dict(type='str', default='http://127.0.0.1'),
         api_port=dict(type='str', default='8697'),
+        validate_certs=dict(type='bool', default='no'),
     )
 
     result = dict(
--- plugins/modules/unix_vmware_desktop_netmgmt.py
+++ plugins/modules/unix_vmware_desktop_netmgmt.py
@@ -104,6 +104,11 @@ options:
         required: false
         default: "8697"
 
+    validate_certs: "no || yes"
+        description:
+            - Validate Certificate it HTTPS connection
+        required: false
+
 author:
     - Adam Magnier (@qsypoq)
 '''
@@ -232,6 +237,7 @@ def run_module():
         setting=dict(type='str', required=False, default=''),
         api_url=dict(type='str', default='http://127.0.0.1'),
         api_port=dict(type='str', default='8697'),
+        validate_certs=dict(type='bool', default='no'),
     )
 
     result = dict(
